5115f583ec7797a6dab42e6aa4f72853a38a4753
Allochka11 <allavelychko11@gmail.com>
2021-11-30 17:11:06 +0200

    adding slides with th sea

---
+++ app.js
@@ -0,0 +1,30 @@
+function slidesPlugin(activeSlide = 3) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active');
+
+    for(const slide of slides) {
+
+
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide)=> {
+            slide.classList.remove('active');
+        })
+    }
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+slidesPlugin(getRandomIntInclusive(0,4));
+
+
